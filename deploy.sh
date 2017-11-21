5287ad58a030869883e5d7e605f223b894fe9ffe
Elliot Barlas <elliotbarlas@gmail.com>
2017-11-20 21:40:42 -0800

    add push state for property changes. add single plant on barlas lane. update geo coords.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 cp . s3://www.barlasgarden.com/v2/ --recursive --exclude "*" --include "*.js" --include "*.svg" --include "*.html"
+aws s3 cp . s3://www.barlasgarden.com/ --recursive --exclude "*" --include "*.js" --include "*.svg" --include "*.html" --include "*/twilight.png"
