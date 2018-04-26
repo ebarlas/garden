d1906a801eefcfdaf1dd45d9d0bff748e1649b1f
Elliot Barlas <elliotbarlas@gmail.com>
2018-04-25 18:29:45 -0700

    add moon phase images to deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 cp . s3://www.barlasgarden.com/ --recursive --exclude "*" --include "*.js" --include "*.svg" --include "*.html" --include "*/twilight.png"
+aws s3 cp . s3://www.barlasgarden.com/ --recursive --exclude "*" --include "*.js" --include "*.svg" --include "*.html" --include "*/twilight.png"  --include "*/moon-phase*.png"
