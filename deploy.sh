0df35bbb90f98e58b0d6049e9958336965191090
Elliot Barlas <elliotbarlas@gmail.com>
2020-04-25 19:58:26 -0700

    multi-line format deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-aws s3 cp . s3://www.barlasgarden.com/ --recursive --exclude "*" --include "*.js" --include "*.svg" --include "*.html" --include "*/twilight.png"  --include "*/moon-phase*.png"
+aws s3 cp . s3://www.barlasgarden.com/ --recursive \
+--exclude "*" \
+--include "favicon*" \
+--include "*.js" \
+--include "*.svg" \
+--include "*.html" \
+--include "*/twilight.png"  \
+--include "*/moon-phase*.png"
