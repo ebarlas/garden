f2f04821672fb970cbcbd00a525c7dbeb0b41ca3
Elliot Barlas <elliotbarlas@gmail.com>
2022-02-27 14:39:07 -0800

    Update garden svg to reflect dead valley violet ceanothus individuals on the hillside.

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,7 @@ aws s3 cp . s3://barlasgarden/garden/ --recursive \
 --include "*.html" \
 --include "*/twilight.png"  \
 --include "*/moon-phase*.png"
+
+aws cloudfront create-invalidation \
+--distribution-id E55WEWI99JZUV \
+--paths /garden/*
